d82291b2eeca064e2b4f41c980b99e8176db0696
Amna Shafiq <amna.shafiq@devsinc.com>
2023-07-26 16:36:55 +0500

    added frontend for the home-page

--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def items_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request,"./order/home.html",context)
+    
