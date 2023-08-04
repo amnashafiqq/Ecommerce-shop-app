312ee7fe33fcbacfbb0f03950fab66f6cfba90b9
Amna Shafiq <amna.shafiq@devsinc.com>
2023-08-04 22:43:21 +0500

    added cart functionality

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import items_list,product_detail
+from .views import checkout,add_to_cart,remove_from_cart
+from . import views
+
+app_name = "order"
 
 urlpatterns=[
-    path('items_list/',items_list,name='itemslist'),
-    path('product/',product_detail,name='productdetail')
+    # path('home/',home,name='itemslist'),
+    # path('product/',product_detail ,name='productdetail'),
+    path('product/<slug>/',views.ItemDetailView.as_view() ,name='productdetail'),
+    path('checkout/',checkout,name='checkout'),
+    path('home/', views.HomeView.as_view(), name='itemslist'),
+    path('add_to_cart/<slug>/',add_to_cart,name ='add-to-cart'),
+    path('remove_from_cart/<slug>/',remove_from_cart,name='remove-from-cart'),
+
+
 
 ]
